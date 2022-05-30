51360c9909a7809cc4a13a9e866e9f4a9179ddd8
monti09 <gautamgjhaj@gmail.com>
2022-05-31 01:09:47 +0530

    added example-modal

---
+++ Example/src/App.vue
@@ -0,0 +1,55 @@
+<template>
+  <div id="app">
+    
+    <VueNiceModal
+     name="'hello'"
+     heading="Modal Header" 
+     @submit="submitBut"
+      >
+       <template slot="modalBody">
+         <div>
+         <label>Content <br></label>
+         </div>
+            <div>
+           <input
+             v-model="text"
+              placeholder="Content input"
+             >
+            </div>
+        </template>
+      </VueNiceModal>
+  </div>
+</template>
+
+
+<script>
+import  VueNiceModal  from 'vue-nice-modal'
+
+export default {
+  name: 'App',
+  components: {
+    VueNiceModal,
+}
+,
+methods:{
+  submitBut() {
+      alert('Button clicked');
+    }
+  }
+}
+</script>
+
+<style>
+/* #app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+} */
+</style>
